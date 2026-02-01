ee0292e25af326c830db5ab323ca89796fc36292
Luan Freitas <luandkg@gmail.com>
2026-02-01 10:13:37 -0300

    ♻️ refactor(main): lista argumentos da função main

--- src/main.d
+++ src/main.d
@@ -1,7 +1,13 @@
 import std.stdio;
 
-void main() {
+void main(string[] args) {
     writeln("");
     writeln("--------------------- QUO :: Commit Semântico @luandkg -----------------------");
     writeln("");
+
+    writeln("Número de argumentos : ",args.length);
+
+    foreach(index, arg; args){
+        writeln("\t Argumento : ",index," = ",arg);
+    }
 }
