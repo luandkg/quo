925990a81f25c2b8f5fe14ff87760a8385435edc
Luan Freitas <luandkg@gmail.com>
2026-02-01 10:26:52 -0300

    ✨ feat(ajuda): implementa função ajuda para fornecer assistência ao usuário

--- src/main.d
+++ src/main.d
@@ -7,9 +7,43 @@ int main(string[] args) {
 
     writeln("Número de argumentos : ",args.length);
 
+    string[] argumentos;
     foreach(index, arg; args){
-        writeln("\t Argumento : ",index," = ",arg);
+        writeln("\t Lendo : ",index," = ",arg);
+        if( index> 0){
+            writeln("\t Adicionando : ",index," = ",arg);
+            argumentos ~=arg;
+        }
+    }
+
+
+    writeln(argumentos);
+
+    bool exibirAjuda = false;
+
+    if(argumentos.length> 0){
+        string primeiro = argumentos[0];
+
+        if(primeiro =="?"){
+            exibirAjuda= true;
+        }
+
+    }else{
+        exibirAjuda= true;
+    }
+
+
+    if(exibirAjuda){
+        menuAjuda();
     }
 
     return 0;
+}
+
+
+void menuAjuda(){
+
+    writeln("\t Ajuda           : ? - Exibe esta ajuda");
+    writeln("\t Formato simples : quo tipo_de_commit mensagem_do_commit");
+    
 }
