99ff81d2c0d4f14532aaaa2f394d906834767c30
Luan Freitas <luandkg@gmail.com>
2026-02-01 11:39:43 -0300

    ✨ feat(constroi_mensagem_de_commit): implementa função que constrói a mensagem de commit a partir dos parâmetros definidos

--- src/commit_semantico.d
+++ src/commit_semantico.d
@@ -14,7 +14,7 @@ void quo_construir(string[] argumentos){
     }else{
 
         Opcional!string commit_tipo = new Opcional!string(strip(argumentos[0]));
-        Opcional!string commit_escopo = new Opcional!string("");
+        Opcional!string commit_escopo = new Opcional!string();
         Opcional!string commit_mensagem = new Opcional!string();
 
         int indice = 1;
@@ -26,6 +26,14 @@ void quo_construir(string[] argumentos){
         writeln("\t COMMIT TIPO     :: ",commit_tipo.get());
         writeln("\t COMMIT ESCOPO   :: ",commit_escopo.get());
         writeln("\t COMMIT MENSAGEM :: ",commit_mensagem.get());
+
+        writeln("");
+
+        string publicar_mensagem = constroi_mensagem_de_commit(commit_tipo,commit_escopo,commit_mensagem);
+
+
+        writeln("\t ",publicar_mensagem);
+
 
     }
 
@@ -50,4 +58,25 @@ int parser_ate_opcionalmente(string[] argumentos,int inicio,string delimitador,O
         resultado.set(texto);
 
         return indice;
+}
+
+
+string constroi_mensagem_de_commit(Opcional!string commit_tipo,Opcional!string commit_escopo,Opcional!string commit_mensagem){
+
+    string publicar_mensagem = "";
+
+    if (commit_tipo.temValor()){
+        publicar_mensagem = commit_tipo.get();
+    }
+
+
+    if(commit_escopo.temValor()){
+        publicar_mensagem ~= "(" ~ commit_escopo.get() ~ ")";
+    }
+
+    if(commit_mensagem.temValor()){
+        publicar_mensagem ~= ": " ~ commit_mensagem.get();
+    }
+
+    return publicar_mensagem;
 }
