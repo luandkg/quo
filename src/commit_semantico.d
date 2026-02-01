fdc5b25b7a28d1188cefb2cd4556bc151ca1c67e
Luan Freitas <luandkg@gmail.com>
2026-02-01 11:21:30 -0300

    ✨ feat(parser_ate_opcionalmente): função para realizar parser reutilizável

--- src/commit_semantico.d
+++ src/commit_semantico.d
@@ -14,7 +14,7 @@ void quo_construir(string[] argumentos){
     }else{
 
         Opcional!string commit_tipo = new Opcional!string(argumentos[0]);
-        string commit_escopo = "";
+        Opcional!string  commit_escopo = new Opcional!string("");
         string commit_mensagem = "";
 
         int indice = 1;
@@ -22,14 +22,8 @@ void quo_construir(string[] argumentos){
         bool mensagem_delimitado = false;
 
 
-        while(indice<argumentos.length && escopo_delimitado==false){
-            if(argumentos[indice] == "-"){
-                escopo_delimitado = true;
-            }else{
-                commit_escopo ~= " " ~ argumentos[indice];
-            }
-            indice+=1;
-        }
+        indice = parser_ate_opcionalmente(argumentos,indice,"-",commit_escopo);
+
 
         while(indice<argumentos.length && mensagem_delimitado==false){
             if(argumentos[indice] == "=="){
@@ -43,12 +37,33 @@ void quo_construir(string[] argumentos){
 
 
         commit_tipo.set(strip(commit_tipo.get()));
-        commit_escopo = strip(commit_escopo);
         commit_mensagem = strip(commit_mensagem);
 
-        writeln("\t COMMIT TIPO     :: ",commit_tipo);
-        writeln("\t COMMIT ESCOPO   :: ",commit_escopo);
+        writeln("\t COMMIT TIPO     :: ",commit_tipo.get());
+        writeln("\t COMMIT ESCOPO   :: ",commit_escopo.get());
         writeln("\t COMMIT MENSAGEM :: ",commit_mensagem);
 
     }
+
+}
+
+int parser_ate_opcionalmente(string[] argumentos,int inicio,string delimitador,Opcional!string resultado){
+
+        int indice = inicio;
+        bool delimitador_encontrado = false;
+        string texto = "";
+
+        while(indice<argumentos.length && delimitador_encontrado==false){
+            if(argumentos[indice] == delimitador){
+                delimitador_encontrado = true;
+            }else{
+                texto ~= " " ~ argumentos[indice];
+            }
+            indice+=1;
+        }
+
+        texto = strip(texto);
+        resultado.set(texto);
+
+        return indice;
 }
